a646722e5a4a644c9d4189ac142e280d1a4bc495
nsohail19 <20noor.sohail@gmail.com>
2025-06-10 21:43:58 -0400

    Lesson 4

--- download.sh
+++ download.sh
@@ -17,3 +17,11 @@ mv single_cell_rnaseq/data .
 rm "$ZIPFILE"
 rm -rf __MACOSX
 rm -rf single_cell_rnaseq
+
+# Web summary
+curl -L "https://www.dropbox.com/scl/fi/skjyyvs9078pbef7s4qvb/ctrl_web_summary.html?rlkey=335gw4qmy29uny5813wg0zakr&dl=1" -o "data/ctrl_web_summary.html"
+curl -L "https://www.dropbox.com/scl/fi/r6n83y57cd7vhc130zm1g/stim_web_summary.html?rlkey=50ur193yfsy5ex9urkmr87h3c&dl=1" -o "data/stim_web_summary.html"
+
+# Metrics summary
+curl -L "https://www.dropbox.com/scl/fi/qnz44ng51ojmhu44acc8g/ctrl_metrics_summary.csv?rlkey=8zx5g1mtn6mrlwpv0syoz3bv4&st=9d0okyhf&dl=1" -o data/ctrl_metrics_summary.csv
+curl -L "https://www.dropbox.com/scl/fi/gaa4sabcbhwqpuogfh0us/stim_metrics_summary.csv?rlkey=h2uokc9r2edeinxtzgrpw3ds4&dl=1" -o data/stim_metrics_summary.csv
